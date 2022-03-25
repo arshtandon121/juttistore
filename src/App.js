9be4a32ad64508fbe4a25529e480852e2eb33b4c
arsh <arshtandon121@gmail.com>
2022-03-25 12:33:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Items from './components/Items.jsx';
+
+function App() {
+  return (
+    <div>
+    <>
+
+
+      <Header/>
+      <Footer/>
+      <Items/>
+    </>
+       
+    </div>
+  );
+   
+  
+}
+
+export default App;
